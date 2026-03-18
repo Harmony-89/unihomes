9b9a48db70ee73fdce6b65216701c21936a36133
Henry Mwirigi <hmwirigi45@gmail.com>
2026-03-19 00:28:28 +0300

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,9 +1,13 @@
 <?php
-$servename = "localhost";
-$username = "root";
-$password = "";
-$database_name = "unihouse";
-$conn = new mysqli($servename, $username, $password, $database_name);
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
 if ($conn->connect_error) {
-    die("Could not connect" . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
+?>
